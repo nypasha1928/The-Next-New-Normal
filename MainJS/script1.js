a7fdf73fa8a42b8de7055b15b3b0b7e4b9d23632
nypasha1928 <71052359+nypasha1928@users.noreply.github.com>
2021-10-03 15:30:41 -0400

    School WebSites

---
+++ MainJS/script1.js
@@ -0,0 +1,82 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+    // Toggle Nav
+    burger.addEventListener('click', () => {
+      // Toggle Nav
+      nav.classList.toggle('nav-active'); 
+        
+      // Animate Links
+      navLinks.forEach((link, index) =>{
+        if (link.style.animation){
+           link.style.animation = '' 
+         } else{
+           link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+        // console.log(index/5);
+         }
+        
+       });
+
+    });
+
+    
+}
+
+navSlide();
+
+// scrolldown fade in animation
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".home-intro");
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+// const sectionOneObserver = new IntersectionObserver(function(
+//   entries,
+//   sectionOneObserver
+// ) {
+//   entries.forEach(entry => {
+//     if (!entry.isIntersecting) {
+//       header.classList.add("nav-scrolled");
+//     } else {
+//       header.classList.remove("nav-scrolled");
+//     }
+//   });
+// },
+// sectionOneOptions);
+
+// sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+});
